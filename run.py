dfcac4ab4e63d4b0fdd1fe10c27e7c841c3d40ad
Víctor Busqué <victorbusque@gmail.com>
2022-03-11 21:32:39 +0100

    more info

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import ssl
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -10,15 +11,17 @@ from os import listdir
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def apply_table(df: pd.DataFrame, total: float) -> float:
     remaining = total
-    retained = 0
+    retained, marginal_rate = 0, 0
     for _, section in df.iterrows():
-        if remaining == 0: break
-        max_amount = float(str(section["Fin Tramo"]).replace(",", ".")) - float(str(section["Inicio Tramo"]).replace(",", "."))
+        if remaining == 0: 
+            marginal_rate = retention_pct
+            break
         retention_pct = float(section["% Tramo"].replace(",", "."))
+        max_amount = float(str(section["Fin Tramo"]).replace(",", ".")) - float(str(section["Inicio Tramo"]).replace(",", "."))
         section_retained = min(remaining, max_amount)
         retained += section_retained * retention_pct
         remaining -= section_retained
-    return retained
+    return retained, marginal_rate
 
 def format_value(val: float):
     return f"{val} €"
@@ -75,22 +78,22 @@ def load_avg_salaries():
         avg_salaries = jsonl(f)
     return avg_salaries
 
-def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc):
+def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc, company_cuota_pct=0.314):
     total_gross = gross_salary_tax + gross_salary_no_tax
         
     # Deductions computation
-    ss_deduction = round(ss_pct*total_gross, 2)
+    ss_cuota = round(ss_pct*total_gross, 2)
     expenses_deduction = 2000
-    total_deduction = ss_deduction + expenses_deduction
+    total_deduction = ss_cuota + expenses_deduction
     personal_post_deduction = 5550
     
     computable_amount = gross_salary_tax - total_deduction
     
-    state_retention = apply_table(state_table, computable_amount)
-    autonomy_retention = apply_table(autonomy_table, computable_amount)
+    state_retention, state_marginal_pct = apply_table(state_table, computable_amount)
+    autonomy_retention, autonomy_marginal_pct = apply_table(autonomy_table, computable_amount)
 
-    state_personal_deduc = apply_table(state_table, personal_post_deduction)
-    autonomy_personal_deduc = apply_table(autonomy_table, personal_post_deduction)      
+    state_personal_deduc, _ = apply_table(state_table, personal_post_deduction)
+    autonomy_personal_deduc, _ = apply_table(autonomy_table, personal_post_deduction)      
     
     state_retention -= state_personal_deduc
     autonomy_retention -= autonomy_personal_deduc
@@ -100,12 +103,21 @@ def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc):
     total_retention = round(state_retention + autonomy_retention - rent_deduc, 2)
     retention_pct = round(total_retention/gross_salary_tax * 100, 2)
 
-    yearly_net = round(gross_salary_tax - ss_deduction - total_retention, 2)
+    yearly_net = round(gross_salary_tax - ss_cuota - total_retention, 2)
     monthly_net = round(yearly_net/12, 2)
     
-    st.markdown(f"Con tu sueldo, te tocará pagar **{format_value(total_retention)}** de IRPF en la declaración, que equivale a una retención del **{retention_pct}%** (sin contar otras posibles deducciones por hijos, aportación a asociaciones, etc).")
+    st.markdown(f"Con tu sueldo, te tocará pagar **{format_value(total_retention)}** de IRPF en la declaración, que equivale a una retención del **{retention_pct}%** (sin contar otras posibles deducciones por hijos, aportación a asociaciones, etc). Además, pagas **{ss_cuota}€** en cuotas a la Seguridad Social.")
     st.markdown(f"Tu sueldo mensual neto (12 pagas) queda en **{ format_value(monthly_net) }**.")
     
+        
+    user_total_state = round( total_retention + ss_cuota, 2)
+    company_cuota_total = round(company_cuota_pct * total_gross, 2)
+    st.write(f"Además de los {user_total_state}€ que pagas en impuestos y cuotas, tu empresa paga otros **{company_cuota_total}€** en cuotas por tí, de modo que en total las instituciones públicas obtienen **{company_cuota_total+user_total_state}€** por tu trabajo.")
+    
+    total_marginal_pct = state_marginal_pct + autonomy_marginal_pct
+    total_marginal_ss_pct = total_marginal_pct +  + ss_pct
+    net_marginal = round(1000*(1-(total_marginal_ss_pct)), 2)
+    st.write(f"Por cada 1000€ que se aumente tu sueldo actualmente, sólamente verías un aumento de **{round(net_marginal/12, 2)}€** netos al mes ({net_marginal}€ al año), es decir, perderías un **{round(total_marginal_ss_pct*100, 2)}%** entre impuestos ({round(total_marginal_pct*100, 2)}%) y cuotas ({round(ss_pct*100, 2)}%).")
     
     avg_salaries = load_avg_salaries()
     avg_salary_state = avg_salaries["state"]
@@ -123,9 +135,7 @@ def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc):
         st.write(f"Tu sueldo está por debajo del sueldo medio de {autonomy}, en concreto un **{-round((total_gross/avg_salary_autonomy-1)*100, 2)}%** menor que la media ({avg_salary_autonomy}€ brutos anuales).")
     else:
         st.write(f"Tu sueldo está en la media de {autonomy}.")
-    
 
-    
     avg_save_rate = 0.057
     recommended_save_rate = 0.15
     average_saves = round(avg_save_rate*monthly_net)
@@ -136,7 +146,7 @@ def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc):
     max_price_rent = round((0.25*yearly_net)/12), round((0.35*yearly_net)/12)
     st.write(f"Con este sueldo deberías poderte permitir pagar un alquiler de **{max_price_rent[0]}€** mensuales de forma cómoda, y los expertos recomiendan que no superes los **{max_price_rent[1]}€** mensuales en un alquiler.")
     
-    return yearly_net, ss_deduction, total_retention
+    return yearly_net, ss_cuota, total_retention
 
 if __name__ == "__main__":
     title = st.title('Calculadora de IRPF')
