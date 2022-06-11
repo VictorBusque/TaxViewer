71a58f3e195e083d6e27718d6cef99dd91d9b815
Víctor Busqué <victorbusque@gmail.com>
2022-06-11 12:50:17 +0200

    updates

--- run.py
+++ run.py
@@ -24,7 +24,10 @@ def apply_table(df: pd.DataFrame, total: float) -> float:
     return retained, marginal_rate
 
 def format_value(val: float):
-    return f"{val} €"
+    return f"{'{:,}'.format(val)} €"
+
+def floatify(val: str) -> float:
+    return float(val.replace(".", "").replace(",", "."))
 
 def get_pie_chart(net, gross_no_taxable, ss_cuota, irpf, divisor=1, name="anual"):
     df = pd.DataFrame()
@@ -70,15 +73,18 @@ def compute_rent_deduc(autonomy_id: str) -> float:
         if rent_deducible:
             monthly_rent_cost = st.number_input("Introduce tu coste de alquiler mensual (lo que pagas tú solamente)", min_value=0, value=500, step=10)
             rent_deduc = min(1000, 0.3*monthly_rent_cost*12)
-    return rent_deduc     
+    return rent_deduc   
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def load_avg_salaries():
-    with open("tables/avg_salary.json", "r", encoding="utf8") as f:
-        avg_salaries = jsonl(f)
-    return avg_salaries
+def compute_ong_deduc() -> float:
+    ong_deducible = st.checkbox("Deducción por aportaciones a asociaciones bonificadas", value=False)
+    deduc = 0
+    if ong_deducible:
+        total_ong = st.number_input("Introduce la cantidad aportada durante el año", min_value=0, value=100, step=10)
+        deduc = 0.8*min(150, total_ong) + 0.35*max(total_ong-150, 0)
+    return deduc
 
-def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc, company_cuota_pct=0.314):
+
+def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc, ong_deduc, company_cuota_pct=0.314):
     total_gross = gross_salary_tax + gross_salary_no_tax
         
     # Deductions computation
@@ -100,57 +106,41 @@ def compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc, comp
     
     state_retention = round(state_retention, 2)
     autonomy_retention = round(autonomy_retention, 2)
-    total_retention = round(state_retention + autonomy_retention - rent_deduc, 2)
+    total_retention = round(state_retention + autonomy_retention - rent_deduc - ong_deduc, 2)
     retention_pct = round(total_retention/gross_salary_tax * 100, 2)
 
     yearly_net = round(gross_salary_tax - ss_cuota - total_retention, 2)
     monthly_net = round(yearly_net/12, 2)
     
-    st.markdown(f"Con tu sueldo, te tocará pagar **{format_value(total_retention)}** de IRPF en la declaración, que equivale a una retención del **{retention_pct}%** (sin contar otras posibles deducciones por hijos, aportación a asociaciones, etc). Además, pagas **{ss_cuota}€** en cuotas a la Seguridad Social.")
+    st.markdown(f"Con tu sueldo, te tocará pagar **{format_value(total_retention)}** de IRPF en la declaración, que equivale a una retención del **{retention_pct}%** (sin contar otras posibles deducciones por hijos, estar a cargo de personas dependientes, etc). Además, pagas **{format_value(ss_cuota)}** en cuotas a la Seguridad Social.")
     st.markdown(f"Tu sueldo mensual neto (12 pagas) queda en **{ format_value(monthly_net) }**.")
     
-        
+    
+    project_pie_chart(yearly_net, gross_salary_no_tax, ss_cuota, total_retention, divisor=1, name="Distribución Anual")
+    
     user_total_state = round( total_retention + ss_cuota, 2)
     company_cuota_total = round(company_cuota_pct * total_gross, 2)
-    st.write(f"Además de los {user_total_state}€ que pagas en impuestos y cuotas, tu empresa paga otros **{company_cuota_total}€** en cuotas por tí, de modo que en total las instituciones públicas obtienen **{company_cuota_total+user_total_state}€** por tu trabajo.")
+    st.write(f"Además de los {format_value(user_total_state)} que pagas en impuestos y cuotas, tu empresa paga otros **{format_value(company_cuota_total)}** en cuotas por tí, de modo que en total las instituciones públicas obtienen **{format_value(round(company_cuota_total+user_total_state, 2))}** por tu trabajo.")
     
     total_marginal_pct = state_marginal_pct + autonomy_marginal_pct
     total_marginal_ss_pct = total_marginal_pct +  + ss_pct
-    net_marginal = round(1000*(1-(total_marginal_ss_pct)), 2)
-    st.write(f"Por cada 1000€ que se aumente tu sueldo actualmente, sólamente verías un aumento de **{round(net_marginal/12, 2)}€** netos al mes ({net_marginal}€ al año), es decir, perderías un **{round(total_marginal_ss_pct*100, 2)}%** entre impuestos ({round(total_marginal_pct*100, 2)}%) y cuotas ({round(ss_pct*100, 2)}%).")
-    
-    avg_salaries = load_avg_salaries()
-    avg_salary_state = avg_salaries["state"]
-    avg_salary_autonomy = avg_salaries[autonomy_id]
-    if total_gross > avg_salary_state:
-        st.write(f"Tu sueldo está por encima del sueldo medio español, en concreto es un **{round((total_gross/avg_salary_state-1)*100, 2)}%** mayor que la media ({avg_salary_state}€ brutos anuales).")
-    elif total_gross < avg_salary_state:
-        st.write(f"Tu sueldo está por debajo del sueldo medio español, en concreto un **{-round((total_gross/avg_salary_state-1)*100, 2)}%** menor que la media ({avg_salary_state}€ brutos anuales).")
-    else:
-        st.write(f"Tu sueldo está en la media española.")
-        
-    if total_gross > avg_salary_autonomy:
-        st.write(f"Tu sueldo está por encima del sueldo medio de {autonomy}, en concreto es un **{round((total_gross/avg_salary_autonomy-1)*100, 2)}%** mayor que la media ({avg_salary_autonomy}€ brutos anuales).")
-    elif total_gross < avg_salary_autonomy:
-        st.write(f"Tu sueldo está por debajo del sueldo medio de {autonomy}, en concreto un **{-round((total_gross/avg_salary_autonomy-1)*100, 2)}%** menor que la media ({avg_salary_autonomy}€ brutos anuales).")
-    else:
-        st.write(f"Tu sueldo está en la media de {autonomy}.")
+    st.write(f"Actualmente, el porcentaje marginal de retención que se te aplica es {total_marginal_pct*100}%, que sumado a lo que pagas de Seguridad Social, te da una retención total de {total_marginal_ss_pct*100}%.")
 
-    avg_save_rate = 0.057
-    recommended_save_rate = 0.15
+    avg_save_rate = 0.114
+    recommended_save_rate = 0.2
     average_saves = round(avg_save_rate*monthly_net)
     recommended_saves = round(recommended_save_rate*monthly_net)
-    st.write(f"Si ahorraras como el español medio ahorrarías **{average_saves}€** mensuales ({average_saves*12}€ al año), aunque los expertos te recomendarían ahorrar por lo menos **{recommended_saves}€** mensuales ({recommended_saves*12}€ al año)")
+    st.write(f"Si ahorraras como el español medio en 2021 ahorrarías **{format_value(average_saves)}** mensuales ({format_value(average_saves*12)} al año), aunque los expertos te recomendarían ahorrar por lo menos **{format_value(recommended_saves)}** mensuales ({format_value(recommended_saves*12)} al año)")
     
             
     max_price_rent = round((0.25*yearly_net)/12), round((0.35*yearly_net)/12)
-    st.write(f"Con este sueldo deberías poderte permitir pagar un alquiler de **{max_price_rent[0]}€** mensuales de forma cómoda, y los expertos recomiendan que no superes los **{max_price_rent[1]}€** mensuales en un alquiler.")
+    st.write(f"Con este sueldo deberías poderte permitir pagar un alquiler de **{format_value(max_price_rent[0])}** mensuales de forma cómoda, y los expertos recomiendan que no superes los **{format_value(max_price_rent[1])}** mensuales en un alquiler.")
     
     return yearly_net, ss_cuota, total_retention
 
 if __name__ == "__main__":
     title = st.title('Calculadora de IRPF')
-    year = st.selectbox("Selecciona el año", options=[2021, 2022], index=0)
+    year = st.selectbox("Selecciona el año", options=[2022], index=0)
     autonomies = [autonomy.title()[:-4].replace("-", " ") for autonomy in listdir(f"tables/{year}") if not autonomy == "state.csv"]
     autonomy = st.selectbox("Selecciona la comunidad autónoma", options=autonomies, index=0)
     autonomy_id = autonomy.lower().replace(' ', '-')
@@ -160,18 +150,20 @@ if __name__ == "__main__":
     autonomy_table = pd.read_csv(f"tables/{year}/{autonomy_id}.csv", sep=";")
     
     # Input
-    gross_salary_tax = st.number_input("Introduce tu sueldo retenible por IRPF", min_value=1000, value=25000, step=100)
-    gross_salary_no_tax = st.number_input("Introduce tu sueldo NO retenible por IRPF (p.e. tarjeta restaurante, transporte, guardería, etc)", min_value=0, value=0)
+    # gross_salary_tax = st.slider("Introduce tu sueldo retenible por IRPF", min_value=1000, value=30000, max_value=200000, step=500)
+    gross_salary_tax = st.number_input("Introduce tu sueldo retenible por IRPF", min_value=1000, value=30000, max_value=200000, step=500)
+    gross_salary_no_tax = st.number_input("Introduce tu sueldo NO retenible por IRPF (p.e. tarjeta restaurante, transporte, guardería, seguro médico, etc)", min_value=0, value=0, step=100)
     
     indef = st.checkbox("Contrato laboral indefinido", value=True)
     if indef: ss_pct = 0.047+0.0155+0.001
     else: ss_pct = 0.047+0.016+0.001
     
-    rent_deduc = compute_rent_deduc(autonomy_id)
+    ong_deduc = compute_ong_deduc()
+    rent_deduc = 0
+    # rent_deduc = compute_rent_deduc(autonomy_id)
     
-    compute = st.button("Calcula")
+    # compute = st.button("Calcula")
+    compute = True
     if compute:
-        yearly_net, yearly_ss, yearly_irpf = compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc)
-        
-        project_pie_chart(yearly_net, gross_salary_no_tax, yearly_ss, yearly_irpf, divisor=1, name="Distribución Anual")
+        yearly_net, yearly_ss, yearly_irpf = compute_irpf(gross_salary_tax, gross_salary_no_tax, ss_pct, rent_deduc, ong_deduc)
         
